68855c9f82fc74f21c27cded648c0b1aefee6843
albertoGrupoAstran <65250786+albertoGrupoAstran@users.noreply.github.com>
2020-09-11 18:10:20 -0500

    se dan estilos en la pagina de servcios

--- js/functions.service.js
+++ js/functions.service.js
@@ -23,10 +23,10 @@ function services(service){
         case 'consultancy':
 
                 options =[{url:'inteligencia-empresarial.html',topic:'Inteligencia Empresarial'},
-                          {url:'optimizacion-portafolio-iniciativas.html',topic:'Optimización del Portafilio de iniciativas'},
-                          {url:'planeacion-seguimiento-proyectos.html',topic:'Planeación y Seguimiento de Proyectos:<br/>Predictivos y/o Adaptativos'},
-                          {url:'gestion-beneficios.html',topic:'Gestión de Beneficios'},
-                          {url:'administracion-cambio.html',topic:'Administración del Cambio'},
+                          {url:'optimizacion-portafolio-iniciativas.html',topic:'Optimización del Portafolio de Iniciativas'},
+                          {url:'planeacion-seguimiento-proyectos.html',topic:'Planeación y Seguimiento De Proyectos:<br/>Predictivos y/o Adaptativos'},
+                          {url:'gestion-beneficios.html',topic:'Gestión De Beneficios'},
+                          {url:'administracion-cambio.html',topic:'Administración Del Cambio'},
                         ];
              
                 
@@ -34,9 +34,9 @@ function services(service){
 
         case 'implementation':
 
-            options =[{url:'implementacion-especialidades.html#gestion',topic:'Gestión de proyectos'},
-            {url:'implementacion-especialidades.html#tecnologia',topic:'Tecnología microsoft para gestión de proyectos'},
-            {url:'implementacion-especialidades.html#administracion',topic:'Administración del cambio'},
+            options =[{url:'implementacion-especialidades.html#gestion',topic:'Gestión De Proyectos'},
+            {url:'implementacion-especialidades.html#tecnologia',topic:'Tecnología Microsoft Para Gestión De Proyectos'},
+            {url:'implementacion-especialidades.html#administracion',topic:'Administración Del Cambio'},
       
           ];
 
@@ -44,14 +44,14 @@ function services(service){
         case 'tecnology':
           
             options =[{url:'tecnologia-inteligencia-empresarial.html',topic:'Inteligencia Empresarial'},
-            {url:'administracion-empresarial-proyectos-ms-ppm.html',topic:'Administración empresarial de proyectos con microsoft'},
-            {url:'administracion-empresarial-proyectos-ms-ppm.html#',topic:'Aceleradores Astran'},
+            {url:'administracion-empresarial-proyectos-ms-ppm.html',topic:'Administración Empresarial De Proyectos Con Microsoft'},
+            {url:'administracion-empresarial-proyectos-ms-ppm.html#',topic:'Aceleradores ASTRAN'},
              ];
         
         break;
         case 'training':    
-                options =[{url:'capacitacion.html',topic:'Prácticas de gestión'},
-                {url:'capacitacion.html',topic:'Competencias digitales'},
+                options =[{url:'capacitacion.html',topic:'Prácticas De Gestión'},
+                {url:'capacitacion.html',topic:'Competencias Digitales'},
                 ];
         
         break;
@@ -76,3 +76,88 @@ function services(service){
     $("#options-services").html(html);
 
 }
+
+//Optimizar el código
+$("#service-consultancy").hover(function(){
+    $("#img1").css({
+        "opacity":".1",
+        "cursor":"pointer",
+    });
+    $("#p1").css({
+        "background-color":"rgba(0,0,0,0)",
+        "color": "#fff"
+    });
+    }, function(){
+    $("#img1").css({
+        "opacity":".5"
+    });
+    $("#p1").css({
+        "background-color":"rgba(255,255,255,0.8)",
+        "color": "#1868a0"
+
+    });
+  
+  });
+  $("#service-implementation").hover(function(){
+    $("#img2").css({
+        "opacity":".1",
+        "cursor":"pointer",
+    });
+    $("#p2").css({
+        "background-color":"rgba(0,0,0,0)",
+        "color": "#fff"
+    });
+    }, function(){
+    $("#img2").css({
+        "opacity":".5"
+    });
+    $("#p2").css({
+        "background-color":"rgba(255,255,255,0.8)",
+        "color": "#1868a0"
+
+    });
+  
+  });
+
+  $("#service-tecnology").hover(function(){
+    $("#img3").css({
+        "opacity":".1",
+        "cursor":"pointer",
+    });
+    $("#p3").css({
+        "background-color":"rgba(0,0,0,0)",
+        "color": "#fff"
+    });
+    }, function(){
+    $("#img3").css({
+        "opacity":".5"
+    });
+    $("#p3").css({
+        "background-color":"rgba(255,255,255,0.8)",
+        "color": "#1868a0"
+
+    });
+  
+  });
+
+  $("#service-training").hover(function(){
+    $("#img4").css({
+        "opacity":".1",
+        "cursor":"pointer",
+    });
+    $("#p4").css({
+        "background-color":"rgba(0,0,0,0)",
+        "color": "#fff"
+    });
+    }, function(){
+    $("#img4").css({
+        "opacity":".5"
+    });
+    $("#p4").css({
+        "background-color":"rgba(255,255,255,0.8)",
+        "color": "#1868a0"
+
+    });
+  
+  });
+
